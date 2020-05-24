3dd910db57bf43042d56b21074060d64af916098
Gigih Iski Prasetyawan <i@gigihiski.com>
2020-05-24 23:05:40 +0700

    :recycle: refactor TextCapitalization handlers in client code

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
 require_once("src/handlers/input/InputString.php");
-require_once("src/entities/fontStyle/FontStyleFactory.php");
+require_once("src/handlers/TextCapitalization/TextCapitalizationFactory.php");
 
 try {
     $input = new InputString($argv);
 
-    $upperCase = FontStyleFactory::create(FontStyleOption::UPPERCASE, $input);
+    $upperCase = TextCapitalizationFactory::create(TextCapitalizationOption::UPPERCASE, $input);
     printf("%s\n", $upperCase->formattedString());
 
-    $alternateUpperLowerCase = FontStyleFactory::create(FontStyleOption::ALTERNATEUPPERLOWERCASE, $input);
+    $alternateUpperLowerCase = TextCapitalizationFactory::create(TextCapitalizationOption::ALTERNATEUPPERLOWERCASE, $input);
     printf("%s\n", $alternateUpperLowerCase->formattedString());
 
-    $normalCase = FontStyleFactory::create(FontStyleOption::NORMALCASE, $input);
+    $normalCase = TextCapitalizationFactory::create(TextCapitalizationOption::ALTERNATEUPPERLOWERCASE, $input);
     printf("%s\n", $normalCase->formattedString());
 } catch (Exception $e) {
     print($e->getMessage());
