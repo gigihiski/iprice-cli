e2a28124264af3591a1981039d8493da8864ff29
Gigih Iski Prasetyawan <i@gigihiski.com>
2020-05-24 23:23:24 +0700

    :white_check_mark: fix text capitalization error message

--- src/utils/exception/TextCapitalizationException.php
+++ src/utils/exception/TextCapitalizationException.php
@@ -3,13 +3,13 @@
 abstract class TextCapitalizationError
 {
     const EMPTY_INPUT = "input is empty";
-    const OPTION_NOT_FOUND = "font style option is not found";
+    const OPTION_NOT_FOUND = "text capitalization option is not found";
 }
 
 class TextCapitalizationException extends Exception
 {
     public function __construct($message)
     {
-        parent::__construct("Font Style Error: {$message}");
+        parent::__construct("Text Capitalization Error: {$message}");
     }
 }
